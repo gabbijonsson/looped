df48234afebf748b00f08508a6b4f8c648d045ac
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2025-05-24 14:29:21 +0200

    update router base

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";import Index from "./pages/Index";import Login from "./pages/Login";import NotFound from "./pages/NotFound";
+import { AuthProvider } from "./contexts/AuthContext";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -13,9 +16,11 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/looped">
           <Routes>
-                        <Route path="/" element={<Index />} />            <Route path="/login" element={<Login />} />            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
